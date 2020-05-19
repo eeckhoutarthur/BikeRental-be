fef3dc516379a787ad21104b63abce18bda05686
Arthur Eeckhout <arthur.eeckhout@student.hogent.be>
2020-05-19 23:11:27 +0200

    Aanmaken van unit test project
    Testen uitschrijven voor de BikeController

---
+++ BikeRentalTest/Controllers/BikesControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using BikeRentalTest.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Moq;
+using ProjectBikeRental.Controllers;
+using ProjectBikeRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace BikeRentalTest.Controllers
+{
+    public class BikesControllerTest
+    {
+        private readonly BikesController _bikesController;
+        private readonly DummyDbContext _dbContext;
+        private readonly Mock<IBikeRepository> _bikeRepo;
+
+        public BikesControllerTest()
+        {
+            _dbContext = new DummyDbContext();
+            _bikeRepo = new Mock<IBikeRepository>();
+            _bikesController = new BikesController(_bikeRepo.Object);
+        }
+
+        [Fact]
+        public void Get_GeeftBikeMetOvereenkomstigeId()
+        {
+            _bikeRepo.Setup(s => s.GetBy(1)).Returns(_dbContext.alleBikes[0]);
+            var result = Assert.IsType<ActionResult<Bike>>(_bikesController.Get(1));
+            Assert.Equal("Tarmac", result.Value.Name);
+        }
+
+        [Fact]
+        public void Get_MetOnbestaandId_WerptNotFound()
+        {
+            Assert.IsType<NotFoundResult>(_bikesController.Get(10).Result);
+        }
+
+        [Fact]
+        public void GetAll_GeeftAlleBikesTerug()
+        {
+            _bikeRepo.Setup(s => s.GetAll()).Returns(_dbContext.alleBikes);
+            var result = Assert.IsAssignableFrom<IEnumerable<Bike>>(_bikesController.GetBikes());
+            Assert.Equal(2, result.Count());
+        }
+
+        [Fact]
+        public void CreateBike_MaaktEenFiets()
+        {
+            Bike bike = new Bike("Venge", BrandEnum.Specialized, Groupset.Shimano, BikeType.Road_Bike, true, 10m);
+
+            var result = _bikesController.CreateBike(bike);
+            Assert.IsType<ActionResult<Bike>>(result);
+        }
+    }
+}
---
+++ BikeRentalTest/Data/DummyDbContext.cs
@@ -0,0 +1,31 @@
+﻿using ProjectBikeRental.Models;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeRentalTest.Data
+{
+    public class DummyDbContext
+    {
+        public Orders order { get; }
+        public Bike bike { get; }
+        public Bike bike2 { get; }
+        public Customer customer { get; }
+        public List<Bike> alleBikes { get; }
+
+        public DummyDbContext()
+        {
+            bike = new Bike("Tarmac", BrandEnum.Specialized, Groupset.Shimano, BikeType.Road_Bike, true, 20m);
+            bike2 = new Bike("Venge", BrandEnum.Specialized, Groupset.Shimano, BikeType.Road_Bike, true, 20m);
+            customer = new Customer("Eeckhout", "Arthur", "1234455", "arthur.eeckhout@student.hogent.be");
+            order = new Orders(new DateTime(2020, 02, 13), new DateTime(2020, 02, 16), bike, customer.Email);
+
+            alleBikes = new List<Bike>()
+            {
+                bike,bike2
+            };
+        }
+
+    }
+}
